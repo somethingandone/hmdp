1815c119e7410d562f999d0d8048eab13428860c
somethingandone <2726202933@qq.com>
2024-12-26 00:34:30 +0800

    学学学

---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,23 @@
+package com.hmdp.utils;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class LoginInterceptor implements HandlerInterceptor {
+
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (UserHolder.getUser() == null) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        UserHolder.removeUser();
+    }
+}
